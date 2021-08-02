e4660cf38122af7be8dcd9c5504d73c0b13385ab
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-08-02 07:28:37 +0600

    new commit

--- index.js
+++ index.js
@@ -9,22 +9,18 @@ document.getElementById('logbtn').addEventListener("click",function(){
 const setAllValue=(method)=>{
     let InputValue=document.getElementById(`${method}Input`);
     let oldBalance=document.getElementById(`${method}balance`)
-    let oldTotal=document.getElementById('totalbal')
- if (method=='deposite') {
+ 
     let updateInput=parseInt(InputValue.value)
-    let depositeValue=parseInt(oldBalance.innerHTML)
-        oldBalance.innerText=updateInput+depositeValue;
+    let numberInput=parseInt(oldBalance.innerHTML)
+        oldBalance.innerText=updateInput+numberInput;
     let oldTotal=document.getElementById('totalbal');
        const oldTotalBal=parseInt(oldTotal.innerHTML)
         oldTotal.innerHTML=oldTotalBal+updateInput;
- }
-else{
-let updateWithInput=parseInt(InputValue.value)
-let oldBalance=document.getElementById('withdrawbalance')
-let withdrawValue=parseInt(oldBalance.innerHTML)
-    oldBalance.innerHTML=updateWithInput+withdrawValue;
+ 
+
+
 let withdrawTotal=document.getElementById('totalbal');
    const withdrawTotalBal=parseInt(withdrawTotal.innerHTML)
     withdrawTotal.innerHTML=withdrawTotalBal-updateWithInput;
-}
+
 }
